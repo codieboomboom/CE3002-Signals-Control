5f0dcebf86ee4b725141129f4953062d402eeb09
AnhTuDo1998 <anhtu.do1998@gmail.com>
2019-10-19 12:11:01 +0800

    Integrated Pushbutton

--- IntegrationV1/IntegrationV1.ino
+++ IntegrationV1/IntegrationV1.ino
@@ -8,14 +8,15 @@ Code for CE3002 Demo:
  * Variables Declaration
  */
 
-#define PUSH1 0//low
-#define PUSH2 1//high
+#define PUSH1 12 //low hb push button
+#define PUSH2 13 //high hb push button
 
 #define BLUE 3 // select the pin for the green LED
 #define GREEN 2 // select the pin for the red LED
 #define RED 11 // select the pin for the blue LED
 #define BUZZ 10 //BUZZ to arduino pin 9 
 
+//LCD pins
 #define LCD_RS 9
 #define LCD_EN 8
 #define LCD_D4 4
@@ -25,28 +26,26 @@ Code for CE3002 Demo:
 
 LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
 
-
-/*int groundpin = 8; // write 0 to get ground
-int bluepin = 9; // select the pin for the green LED
-int greenpin = 10; // select the pin for the red LED
-int redpin = 11; // select the pin for the blue LED
-int buzzer = 4; //buzzer to arduino pin 9 
-int grd = 6; // grd for buzzer*/
+//Time keeping
 unsigned long StartTime;
 unsigned long CurrentTime;
 unsigned long ElapsedTime;
-int sig = 0;
-int weakhb;
-int stronghb;
-int flag=0;
-int hbeat=0;
-int realhbeat=1;
-boolean f=true;
 
+//Signals and Flags
+int sig = 0; //sensor output
+int weakhb; //pushbuttonLow
+int stronghb; //pushbuttonHigh
+int flag=0; //up/down
+int hbeat=0; //pulse
+int realhbeat=1; //real hearbeat
+
+//max, min beats
 int maxb;
 int minb;
 
+
 void setup () {
+  Serial.println("Connected");
   pinMode(PUSH1, INPUT);
   pinMode(PUSH2, INPUT);
 
@@ -81,7 +80,6 @@ void setup () {
   }
 
   maxb= minb = 10; //ensure max or min value is at least/at most 10        
-  
   for(int counter = 10; counter > 0; counter--){
     lcd.clear();
     lcd.setCursor(0,0);
@@ -98,124 +96,93 @@ void setup () {
     }
     //reapeat until 10 sec pass
   }
+  Serial.println(maxb); //For debug purpose
+  StartTime = millis();
+  lcd.clear();
+  lcd.print("Reading...");
 }
-
  
 void loop () {
 
+  //For simulation testing | COMMENT OUT
+  maxb = 900;
+  sig= 1000;
+  //sig=analogRead(A0);
 
-  
-  // sig= 1000;
-  sig=analogRead(A0);
-
-
-  weakhb=analogRead(A1); //read low = low hb
-  stronghb=analogRead(A2); //read low = high hb
-  if(sig >=(maxb-50) && flag ==0 && f )//&& weakhb<500 && stronghb<500 )
-  {
-      
-    analogWrite (RED,  100);      //red
-    delay(10);
-    tone(BUZZ, 1000); // Send 1KHz sound signal...
-    analogWrite (RED,  0);      //red
-    delay(80);        // ...for 80m sec
-    
-    noTone(BUZZ);     // Stop sound...
-    delay(10);        // ...for 10m sec 
-   
-    flag=1;
-    hbeat++;
-    
-  }
-  else if ( sig <=(maxb-50) && flag == 1)
-          flag=0;
-
-
-          
+  weakhb=digitalRead(PUSH1); //read button LOW
+  stronghb=digitalRead(PUSH2); // read button HIGH
+  if (weakhb == stronghb){
+    if(sig >=(maxb-50) && flag ==0)//&& weakhb<500 && stronghb<500 )
+    {
         
-        CurrentTime = millis();
-        ElapsedTime = CurrentTime - StartTime;
-        //Serial.println(hbeat);
-        if (ElapsedTime>=10000){
-        lcd.clear();
-        realhbeat=hbeat*6;
-        hbeat=0;
-        StartTime = millis(); 
-        CurrentTime=0;
-        ElapsedTime=0;
-        lcd.print("HeartBeat:");
-        lcd.setCursor(0,1);
-        lcd.print(realhbeat);
-        lcd.print(" BPM");
-        //Serial.println(realhbeat);
- }
+      analogWrite (RED,  100);      //red
+      delay(10);
+      tone(BUZZ, 1000); // Send 1KHz sound signal...
+      analogWrite (RED,  0);      //red
+      delay(80);        // ...for 80m sec
+      
+      noTone(BUZZ);     // Stop sound...
+      delay(10);        // ...for 10m sec 
+    
+      flag=1;
+      hbeat++;
+      
+    }
+    //for simulation purpose | COMMENT OUT
+    delay(1000);
+    maxb = 1100;
+
+    if ( sig <=(maxb-50) && flag == 1)
+      flag=0;
+    
+    CurrentTime = millis();
+    ElapsedTime = CurrentTime - StartTime;
+    //Serial.println(hbeat);
+    if (ElapsedTime>=10000){
+    lcd.clear();
+    realhbeat=hbeat*6;
+    hbeat=0;
+    StartTime = millis(); 
+    CurrentTime=0;
+    ElapsedTime=0;
+    lcd.print("HeartBeat:");
+    lcd.setCursor(0,1);
+    lcd.print(realhbeat);
+    lcd.print(" BPM");
+    //Serial.println(realhbeat);
+      }  
+}
  
         // Serial.println(ElapsedTime);
  
         //delay(1000);
         
- Serial.println(sig);
+ //Serial.println(sig);
+  else{
+    if(weakhb)
+    {
+        analogWrite (BLUE,  255);      //blue
+        delay(10);
+        tone(BUZZ, 1000); // Send 1KHz sound signal...
+        delay(20);        // ...for 80m sec
+        //noTone(BUZZ);     // Stop sound...
+        delay(10);        // ...for 10m sec 
+        lcd.clear();
+        lcd.print("Weak heartbeat!!!");
+    }
 
- if(weakhb<300)
- {
-    f=false;
-    analogWrite (RED,  0);      //blue
-    delay(10);
-    tone(BUZZ, 1000); // Send 1KHz sound signal...
-    delay(20);        // ...for 80m sec
-    //noTone(BUZZ);     // Stop sound...
-    delay(10);        // ...for 10m sec 
-    weakhb=analogRead(A1); //read low = low hb
-    stronghb=analogRead(A2); //read low = high hb
-   
-  
- }
-
- if (stronghb<300)
- {
-  f=false;
-    analogWrite (RED,  100);      //green
-    delay(10);
-    tone(BUZZ, 1000); // Send 1KHz sound signal...
-    delay(20);        // ...for 80m sec
-   // noTone(BUZZ);     // Stop sound...
-    delay(10);        // ...for 10m sec 
-    weakhb=analogRead(A1); //read low = low hb
-    stronghb=analogRead(A2); //read low = high hb
-    
- }
-  if(weakhb>=300||stronghb>=300){
-    f=true;
-    noTone(BUZZ);     // Stop sound...
-    delay(10);        // ...for 10m sec 
-
-    
+    else if (stronghb)
+    {
+        analogWrite (RED,  255);      //green
+        delay(68);
+        tone(BUZZ, 1000); // Send 1KHz sound signal...
+        delay(60);        // ...for 80m sec
+        noTone(BUZZ);     // Stop sound...
+        delay(20);        // ...for 10m sec 
+        lcd.clear();
+        lcd.print("Strong heartbeat!!!");   
+    }
+    analogWrite(BLUE, 0);
+    analogWrite(RED, 0);
   }
-  
-    
-
-
-
-
-
-
-
-
-
-
-
-
- //Serial.println(maxb);
-//  Serial.println(minb);
-
- // analogWrite (greenpin,   100);  //green
-  //analogWrite (bluepin, 100);    //blue
- // analogWrite (redpin,  100);      //red
-
-  /*  tone(buzzer, 1000); // Send 1KHz sound signal...
-      delay(500);        // ...for 1 sec
-      noTone(buzzer);     // Stop sound...
-      delay(500);        // ...for 1sec 
-      */
- 
 }
